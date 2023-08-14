1f84e7c4949dd321f4c0743f234496228c35dc20
Milind Nair <nairmilind3@gmail.com>
2023-08-14 23:30:16 +0530

    removed unused imports

--- frontend/src/components/UserHistoryDialog.js
+++ frontend/src/components/UserHistoryDialog.js
@@ -5,11 +5,9 @@ import { styled } from "@mui/material/styles";
 import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
 import DialogContent from "@mui/material/DialogContent";
-import DialogActions from "@mui/material/DialogActions";
 import { List, ListItem, ListItemText } from "@mui/material";
 import IconButton from "@mui/material/IconButton";
 import CloseIcon from "@mui/icons-material/Close";
-import Typography from "@mui/material/Typography";
 
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
   "& .MuiDialogContent-root": {
