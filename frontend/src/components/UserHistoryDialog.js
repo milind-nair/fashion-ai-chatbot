88bbaad6d42eb4778e7307dfe78ebccb82d4e9b3
Milind Nair <nairmilind3@gmail.com>
2023-08-16 20:10:35 +0530

    removed unused imports

--- frontend/src/components/UserHistoryDialog.js
+++ frontend/src/components/UserHistoryDialog.js
@@ -1,6 +1,4 @@
 import * as React from "react";
-import PropTypes from "prop-types";
-import Button from "@mui/material/Button";
 import { styled } from "@mui/material/styles";
 import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
@@ -20,7 +18,6 @@ const BootstrapDialog = styled(Dialog)(({ theme }) => ({
 
 function BootstrapDialogTitle(props) {
   const { children, onClose, ...other } = props;
-
   return (
     <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
       {children}
