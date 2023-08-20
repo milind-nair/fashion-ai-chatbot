93fabad69d4c3fdaebbc842a1fb6f1407b390f32
Milind Nair <nairmilind3@gmail.com>
2023-08-20 10:05:52 +0530

    cleanup

--- frontend/src/constants/constants.js
+++ frontend/src/constants/constants.js
@@ -18,13 +18,3 @@ export const conversations = [
     history: user3,
   },
 ];
-
-// export const jsonData = {
-//   name: "Apple MacBook Pro 16",
-//   data: {
-//     year: 2019,
-//     price: 1849.99,
-//     "CPU model": "Intel Core i9",
-//     "Hard disk size": "1 TB",
-//   },
-// };
