4e96fc6cff09a050524cc65fba54f399fe13322b
theGarimaAgrawal <higarima1@gmail.com>
2023-08-20 14:15:33 +0530

    modified  json return

--- backend/product_handle.py
+++ backend/product_handle.py
@@ -1,44 +1,40 @@
-# import all libraries
+# Import necessary libraries
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import nltk
 from nltk import word_tokenize, pos_tag
+from flask import Flask, jsonify
 
-# main function define
-
-
+# Main function definition
 def main(sql):
-
+    # Read the product dataset
     productDf = pd.read_csv('C:/Users/GarimaJi/.vscode/nlp/fashion-ai-chatbot/backend/dataset/flipkartProductDataset.csv')
     preprocessedSql = sql.lower()
     preprocessedProducts = productDf.applymap(
         lambda x: x.lower() if isinstance(x, str) else x)
-    # type(productDf)
 
     # Tokenize the sentence into words
     words = word_tokenize(preprocessedSql)
 
     # Perform part-of-speech tagging
     pos_tags = pos_tag(words)
-    print(pos_tags)
 
     # Find adjectives and nouns and combine them
     adjective_noun_pairs = []
     i = 0
-    while (i != len(pos_tags)):
+    while i != len(pos_tags):
         if pos_tags[i][1].startswith('JJ'):
             j = i
-            while (pos_tags[j][1].startswith('NN') == False):
-                # print(pos_tags[j][0],pos_tags[j][1])
+            while not pos_tags[j][1].startswith('NN'):
                 j += 1
 
             r = []
-            for k in range(i, j+1):
-                if (pos_tags[k][1].startswith('JJ') or pos_tags[k][1].startswith('RB') or pos_tags[k][1].startswith('NN')):
+            for k in range(i, j + 1):
+                if pos_tags[k][1].startswith('JJ') or pos_tags[k][1].startswith('RB') or pos_tags[k][1].startswith('NN'):
                     r.append(pos_tags[k][0])
             adjective_noun_pairs.append(r)
-            i = j+1
+            i = j + 1
 
         elif pos_tags[i][1].startswith('NN'):
             adjective_noun_pairs.append([pos_tags[i][0]])
@@ -46,12 +42,12 @@ def main(sql):
         else:
             i += 1
 
-    print("Adjective-Noun Pairs:", adjective_noun_pairs[0])
-
-    # Create TF-IDF vectorizer
     # Create TF-IDF vectorizer
     preprocessedProducts['Combined'] = preprocessedProducts['product_category_tree'].str.cat(
         preprocessedProducts['product_specifications'], sep=' ')
+
+    # Prepare the list of recommended products
+    recommended_products = []
     for pair in adjective_noun_pairs:
         vectorizer = TfidfVectorizer()
         productVectors = vectorizer.fit_transform(preprocessedProducts['Combined'].fillna(''))
@@ -63,13 +59,34 @@ def main(sql):
 
         recommended_urls = ranked_products['product_url'].tolist()
         recommended_prices = ranked_products['retail_price'].tolist()
-        recommendations=[]
-        recommendations.extend(list(zip(recommended_urls[:10], recommended_prices[:10])))
+        recommended_names = ranked_products['product_name'].tolist()
+        recommended_img_url = ranked_products['image'].tolist()
+        #print(recommended_img_url)
+        recommendations = []
+        recommendations.extend(list(zip(recommended_urls[:10], recommended_prices[:10], recommended_names[:10])))
+
+        recommended_products.extend(recommendations)
+
+    recommended_products_list = []
+    for url, price, name in recommended_products:
+        recommended_product_dict = {
+            'name': name,
+            'url': url,
+            'price': price
+        }
         
-    return recommendations
-        
+        recommended_products_list.append(recommended_product_dict)
+     
+         
+     
+
+    #print((recommended_img_url[0].split(',')[0].replace("[","")).replace('"',""))
+    #final_dict=[]
+    return jsonify(recommended_products_list)
+
 if __name__ == "__main__":
     query = 'Blue Kurta with black jeans should go well'
-    recommended_products = main(query)
-    for i, (product_url, product_price) in enumerate(recommended_products, start=1):
-        print(f"Rank {i}: URL: {product_url}, Price: {product_price}")
+    recommended_products_json = main(query)
+    #print(recommended_products_dict)
+    # Convert list of tuples to list of dictionaries
+
